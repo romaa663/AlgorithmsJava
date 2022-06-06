ca02e3d436a6987da1f8cca1f310ccffb007a0ce
Platonov Roman <romaa.6633@gmail.com>
2022-06-06 11:25:23 +0400

    Первый commit

---
+++ src/main/sorts/selection_sort/SelectionSort.java
@@ -0,0 +1,18 @@
+package main.sorts.selection_sort;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] ints = new int[20];
+        Random random = new Random();
+        for (int i = 0; i < ints.length; i++) {
+            ints[i] = random.nextInt(100);
+        }
+        System.out.println(Arrays.toString(ints));
+        for (int i = 0; i <  ; i++) {
+
+        }
+    }
+}
